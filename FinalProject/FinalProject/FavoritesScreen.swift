fbe757c29fc1bd943f8c99e23e8dac833bb8ec50
selcukarz <ariozselcuk@gmail.com>
2024-05-22 16:11:07 +0300

    Screens and files added

---
+++ FinalProject/FinalProject/FavoritesScreen.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesScreen.swift
+//  FinalProject
+//
+//  Created by Selçuk Arıöz on 22.05.2024.
+//
+
+import UIKit
+
+class FavoritesScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FinalProject/FinalProject/MainScreen.swift
+++ FinalProject/FinalProject/MainScreen.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
